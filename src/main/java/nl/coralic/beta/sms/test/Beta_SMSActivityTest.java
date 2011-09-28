1e371af819e1500e708096ffbc36badf913a7364
Armin <armin@coralic.nl>
2011-09-28 17:19:50 +0200

    spelling error fixed

---
+++ src/main/java/nl/coralic/beta/sms/test/Beta_SMSActivityTest.java
@@ -0,0 +1,29 @@
+package nl.coralic.beta.sms.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import nl.coralic.beta.sms.*;
+
+public class Beta_SMSActivityTest extends
+	ActivityInstrumentationTestCase2<Beta_SMS>
+{
+
+    Beta_SMS activity;
+
+    public Beta_SMSActivityTest()
+    {
+	super("nl.coralic.beta.sms", Beta_SMS.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception
+    {
+	super.setUp();
+	activity = this.getActivity();
+    }
+
+    public void testActivity()
+    {
+	Beta_SMS activity = getActivity();
+	assertNotNull(activity);
+    }
+}
